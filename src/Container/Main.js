4f90309e374061977555c9ec687576b059daed4c
AngelMarudhu <angeldeveloper09@gmail.com>
2024-12-25 14:33:45 +0530

    optimize

--- src/Container/Main.js
+++ src/Container/Main.js
@@ -8,27 +8,11 @@ const Main = () => {
   const ref = React.useRef(null);
   const isInView = useInView(ref, { once: true });
   const text = "MarudhuPandiyan S";
-  const headline = "Crafting Code, Building Dreams";
+  // const headline = "";
 
   return (
     <section className="top">
-      <h2 className="top_heading">
-        <AnimatePresence>
-          {headline.split("").map((char, i) => (
-            <motion.span
-              ref={ref}
-              key={i}
-              initial={{ opacity: 0, x: -1 }}
-              animate={isInView ? { opacity: 1, x: 0 } : {}}
-              exit="hidden"
-              transition={{ duration: 0.5, delay: i * 0.06 }}
-              className="text-xl text-center sm:text-4xl font-bold tracking-tighter md:text-6xl md:leading-[4rem]"
-            >
-              {char === " " ? <span>&nbsp;</span> : char}
-            </motion.span>
-          ))}
-        </AnimatePresence>
-      </h2>
+      <h2 className="top_heading"> Crafting Code, Building Dreams</h2>
 
       <div className="hero">
         <div className="hero_intro">
